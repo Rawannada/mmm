2d34f46431416a580a3f8664890301de84c26227
Rawannada <125425680+Rawannada@users.noreply.github.com>
2025-08-13 13:05:09 +0300

    app

---
+++ mm.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd 
+#title
+st.title("first streamlit app")
+name = st.text_input("enter your name",placeholder="type here ...")
+# if name != "type here ...":
+#     st.write(f"hello {name}🥰")
+
+st.write(f"hello {name}🥰")
+upload =st.file_uploader("upload a csv file",type=["csv"])
+if upload:
+    df=pd .read_csv(upload)
+    st.write("dataframe:")
+    st.dataframe(df.head())
+    
+    
+    st.download_button(
+        label="download csv",
+        data=df.to_csv(index=False),
+        file_name="dowenloaded_data.csv",
+        mime="text/csv"
+    )
+    
+    
+    
